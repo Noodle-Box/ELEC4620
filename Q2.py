38a7842b22a7418681afc4a4b082089ed70dba1e
Tevo <84370227+Noodle-Box@users.noreply.github.com>
2024-03-01 01:20:19 +0000

    Got Q2 sample code

--- Q2.py
+++ Q2.py
@@ -11,6 +11,20 @@ while cap.isOpened():
 
     img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     img = img[0:-200, 400:-400]
+
+    img_sobel_x = cv.Sobel(img, cv.CV165, 1, 0, None, 3)
+    img_sobel_y = cv.Sobel(img, cv.CV165, 1, 0, None, 3)
+    img_sobel = np.abs(img_sobel_x) + np.abs(img_sobel_y)
+
+    img_sobel_norm = (img_sobel - np.min(img_sobel)) / (np.max(img_sobel) - np.min(img_sobel))
+    cv.imshow("Image", img_show_norm)
+    c = cv.waitKey(16)
+    if c == ord('q'):
+        break
+
+
+
     
+
 
 
