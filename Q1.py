144ee320b5a78adfeeead3098b9c9e44c290d45d
Tevo <84370227+Noodle-Box@users.noreply.github.com>
2024-03-01 00:29:11 +0000

    idk what im doing

--- Q1.py
+++ Q1.py
@@ -5,21 +5,21 @@ import numpy as np
 img = cv.imread("StreetSigns/Image0.jpg")
 
 #convert to grey scale
-img_gray=cv.cvtColour(img, cv.COLOUR_BGR2GRAY)
+#img_gray=cv.cvtColour(img, cv.COLOUR_BGR2GRAY)
 
 
 #edge detector. Look at OpenCV documentation
 #edges = cv.Canny(img_gray, 150, 250)
-grad_x = cv.Sobel(img_gray, cv.CV_16F, 1, 0, ksize = 3)
-grad_x = (grad_x - np.min(grad_x))/(np.max(grad_x - np.min(grad_x)))
+#grad_x = cv.Sobel(img_gray, cv.CV_16F, 1, 0, ksize = 3)
+#grad_x = (grad_x - np.min(grad_x))/(np.max(grad_x - np.min(grad_x)))
 
-grad_y = cv.Sobel(img_gray, cv.CV_16F, 1, 0, ksize = 3)
-grad_y = (grad_y - np.min(grad_y))/(np.max(grad_y - np.min(grad_y)))
+#grad_y = cv.Sobel(img_gray, cv.CV_16F, 1, 0, ksize = 3)
+#grad_y = (grad_y - np.min(grad_y))/(np.max(grad_y - np.min(grad_y)))
 
 #combine!
-grad_xy_norm = (grad)
+#grad_xy_norm = (grad)
 
 #print image and show
-print(img.shape)
-cv.imshow("Image", grad_x)
-cv.waitKey()
+#print(img.shape)
+#cv.imshow("Image", grad_x)
+#cv.waitKey()
